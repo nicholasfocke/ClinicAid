49b8aa45acbcdc839b6d794b21f14979e388a53c
Hugo Stankowich <hstankowich@gmail.com>
2025-07-31 00:23:56 -0300

    Adicionado autenticação de login

--- pages/admin/pacientes/paginapaciente.tsx
+++ pages/admin/pacientes/paginapaciente.tsx
@@ -1,8 +1,27 @@
 import ProtectedRoute from '@/components/layout/ProtectedRoute';
 import breadcrumbStyles from '@/styles/Breadcrumb.module.css';
 import styles from '@/styles/admin/pacientes/paginapaciente.module.css';
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { auth } from '@/firebase/firebaseConfig';
+import { useRouter } from 'next/router';
 
-export default function PaginaPaciente() {
+const PaginaPaciente = () => {
+    const router = useRouter();
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+        if (currentUser) {
+            setUser(currentUser);
+        } else {
+            router.push('/auth/login');
+        }
+        });
+
+        return () => unsubscribe();
+    }, [router]);
+
   return (
     <ProtectedRoute>
       <div className={styles.container}>
@@ -16,3 +35,5 @@ export default function PaginaPaciente() {
     </ProtectedRoute>
   );
 }
+
+export default PaginaPaciente;
